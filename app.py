b6d15929df507797849d2b1434102f0f6e4f3de7
Dharmil <dharmil132@gmail.com>
2025-12-28 20:52:56 +0530

    App.pu

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-import psycopg2
+from flask import Flask, redirect, render_template, request, url_for, jsonify
+
+
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+
+# only run the app if this file is executed directly
+if __name__ == "__main__":
+    app.run(debug=True)
