7f021f41fb4a6446a0087ddcec13f135f0fa3b3c
Dharmil <dharmil132@gmail.com>
2026-01-02 21:41:01 +0530

    Refactor user authentication flow; add getUserByEmail function and remove dashboard template

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, redirect, render_template, request, url_for, jsonify
+from flask import Flask, render_template,session
 from service.userservice import login_service
 from routes.user_routes import user_bp
 
-# Initialize the Flask application
 app = Flask(__name__)
+app.secret_key = "an12eadf234f" 
 
 app.register_blueprint(user_bp, url_prefix='/login')
 
@@ -11,6 +11,5 @@ app.register_blueprint(user_bp, url_prefix='/login')
 def home():
     return render_template("home.html")
 
-# only run the app if this file is executed directly
 if __name__ == "__main__":
     app.run(debug=True)
--- database/userdao.py
+++ database/userdao.py
@@ -1,4 +1,5 @@
 from database.connection import get_connection as gc
+from modal.User import User
 
 def existUser(email):
     query = "SELECT * FROM users WHERE email = %s"
@@ -36,3 +37,14 @@ def saveUser(user):
     finally:
         cur.close()
         conn.close()
+
+def getUserByEmail(email):
+    query = "SELECT id, name, email,password FROM users WHERE email = %s"
+    cur=gc().cursor()
+    cur.execute(query, (email,))
+    result = cur.fetchone()
+    if result is not None:
+        user=User(result[0],result[1],result[2],result[3])
+        return user
+    else:
+        return None
